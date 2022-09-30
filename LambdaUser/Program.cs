f40480fd609184b665c5931e1fc71498bed1cb83
Payal Sharma <https://github.com/29payal>
2022-09-30 18:54:00 +0530

    [Payal] User-UC13

---
+++ LambdaUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UserDetails> listPerson = new List<UserDetails>();
+            UserDetails.AddRecords(listPerson);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LambdaUser/UserDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaUser
+{
+    public class UserDetails
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Password;
+        public int PhoneNumber;
+        public UserDetails(string firstName, string lastName, string email, string password, int phoneNumber )
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+        }
+        public static void AddRecords(List<UserDetails> user)
+        {
+            user.Add(new UserDetails("Payal" , "Sharma" ,"abc@gmail.com" , "ksgkhhcb" , 1234567890));
+
+            foreach (var person in user.OrderBy(x => x.FirstName))
+            {
+                Console.WriteLine( "FirstName     : " +person.FirstName);
+                Console.WriteLine("LastName      : " +person.LastName);
+                Console.WriteLine( "Email         : " +person.Email);
+                Console.WriteLine("Password       : " +person.Password);
+                Console.WriteLine("Mobile Number  : " +person.PhoneNumber);
+            }
+        }
+
+    }
+}
